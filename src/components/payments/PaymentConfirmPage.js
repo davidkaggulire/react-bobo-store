1e4c67b34fd64a60966bbd9d5758a0ce1b53f8ce
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-18 11:48:42 +0300

    confirm payment screen

--- src/components/payments/PaymentConfirmPage.js
+++ src/components/payments/PaymentConfirmPage.js
@@ -4,38 +4,12 @@ import Card from "../UI/Card";
 import classes from "./PaymentConfirmPage.module.css";
 
 const PaymentConfirmPage = () => {
-  // const cartItems = useSelector((state) => state.cart.items);
-
-  //  get total price
-  // const total = cartItems
-  //   .map((item) => item.totalPrice)
-  //   .reduce((prev, curr) => prev + curr, 0);
 
   return (
     <div className={classes.receipt_section}>
       <Card>
         <p>Payment Confirmed</p>
         <p>Yay! Order placed! 🛒 You will receive an email confirmation confirming your order.</p>
-        <h3>Items purchased</h3>
-        {/* {cartItems.length > 0 &&
-          cartItems.map((item) => (
-            <Orders
-              key={item.id}
-              id={item.id}
-              title={item.name}
-              image={item.image}
-              price={item.price}
-              quantity={item.quantity}
-              description={item.description}
-              totalPrice={item.totalPrice}
-            />
-          ))} */}
-        {/* <div className={classes.total}>
-          <h3>Total</h3>
-          <p>
-            $ <span>{total}</span>
-          </p>
-        </div> */}
       </Card>
     </div>
   );
