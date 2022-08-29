b3461e2a31bb7c292fc3adbf37ae1cdb7bf855b2
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-30 00:04:44 +0300

    edit cart button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,22 +2,31 @@ import { useSelector } from "react-redux";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 import OrderButton from "../Orders/OrderButton";
-
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
 
+  const navigate = useNavigate();
+
+  const linkToProduct = () => {
+    navigate("../products", true)
+  }
+
   return (
     <div className={classes.cartpage}>
       <div className={classes.heading}>
-      <h2 className={classes.title}>Shopping Cart</h2>
-      { cartItems.length > 0 && <OrderButton />}
+        <h2 className={classes.title}>Shopping Cart</h2>
+        {cartItems.length > 0 && <OrderButton />}
       </div>
-      
+
       {cartItems.length === 0 && (
-        <p className={classes.empty}>No products added to cart</p>
+        <div>
+          <p className={classes.empty}>No products added to cart</p>
+          <button className={classes.back} onClick={linkToProduct}>Back to Products</button>
+        </div>
       )}
-      
+
       {cartItems.length > 0 &&
         cartItems.map((item) => (
           <CartItem
