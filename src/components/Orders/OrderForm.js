706084e1b1f5c8c5f35166ca57ffb6746488d02e
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-02 00:50:46 +0300

    add react toastify

--- src/components/Orders/OrderForm.js
+++ src/components/Orders/OrderForm.js
@@ -1,220 +1,169 @@
-import { useEffect, useReducer, useRef, useState } from "react";
+import { useState, Fragment } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { contactActions } from "../../store/contact-slice";
-import Input from "../UI/Input";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./OrderForm.module.css";
-import {
-  countryReducer,
-  addressReducer,
-  cityReducer,
-  firstNameReducer,
-  lastNameReducer,
-} from "./orderreducer";
+
+import { toast, ToastContainer } from "react-toastify";
 
 const OrderForm = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
 
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [values, setValues] = useState({
+    country: "",
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+  });
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
   const dispatch = useDispatch();
 
-  const [countryState, dispatchCountry] = useReducer(countryReducer, {
-    value: "",
-    isValid: null,
-  });
+  const handleChange = (event) => {
+    // console.log(event.target.value);
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-  const [firstNameState, dispatchFirstName] = useReducer(firstNameReducer, {
-    value: "",
-    isValid: null,
-  });
+  const validate = () => {
+    const { country, firstName, lastName, address, city } = values;
 
-  const [lastNameState, dispatchLastName] = useReducer(lastNameReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const [addressState, dispatchAddress] = useReducer(addressReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const [cityState, dispatchCity] = useReducer(cityReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const countryInputRef = useRef();
-  const firstNameInputRef = useRef();
-  const lastNameInputRef = useRef();
-  const addressInputRef = useRef();
-  const cityInputRef = useRef();
-
-  const { isValid: countryIsValid } = countryState;
-  const { isValid: firstNameIsValid } = firstNameState;
-  const { isValid: lastNameIsValid } = lastNameState;
-  const { isValid: addressIsValid } = addressState;
-  const { isValid: cityIsValid } = cityState;
-
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      //   console.log("Checking form validity");
-
-      setFormIsValid(
-        countryIsValid &&
-          firstNameIsValid &&
-          lastNameIsValid &&
-          addressIsValid &&
-          cityIsValid
+    if (country.length < 1) {
+      toast.error("country should contain at least 3 characters", toastOptions);
+      return false;
+    } else if (firstName.length < 2) {
+      toast.error(
+        "firstName should be equal or greater than 3 characters.",
+        toastOptions
       );
-    }, 500);
+      return false;
+    } else if (lastName.length < 2) {
+      toast.error(
+        "lastName should be equal or greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (address.length < 2) {
+      toast.error(
+        "address should be equal or greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (city.length < 2) {
+      toast.error(
+        "city should be equal or greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    }
 
-    // this runs as a clean up function
-    return () => {
-      //   console.log("CLEAN UP");
-      clearTimeout(identifier);
-    };
-  }, [
-    countryIsValid,
-    firstNameIsValid,
-    lastNameIsValid,
-    addressIsValid,
-    cityIsValid,
-  ]);
-
-  const countryChangeHandler = (event) => {
-    dispatchCountry({ type: "USER_INPUT", val: event.target.value });
-  };
-
-  const firstNameChangeHandler = (event) => {
-    dispatchFirstName({ type: "USER_INPUT", val: event.target.value });
-  };
-
-  const lastNameChangeHandler = (event) => {
-    dispatchLastName({ type: "USER_INPUT", val: event.target.value });
-  };
-
-  const addressChangeHandler = (event) => {
-    dispatchAddress({ type: "USER_INPUT", val: event.target.value });
-  };
-
-  const cityChangeHandler = (event) => {
-    dispatchCity({ type: "USER_INPUT", val: event.target.value });
-  };
-
-  const validateCountryHandler = () => {
-    dispatchCountry({ type: "INPUT_BLUR" });
-  };
-
-  const validateFirstNameHandler = () => {
-    dispatchFirstName({ type: "INPUT_BLUR" });
-  };
-
-  const validateLastNameHandler = () => {
-    dispatchLastName({ type: "INPUT_BLUR" });
-  };
-
-  const validateAddressHandler = () => {
-    dispatchAddress({ type: "INPUT_BLUR" });
-  };
-
-  const validateCityHandler = () => {
-    dispatchCity({ type: "INPUT_BLUR" });
+    return true;
   };
 
   const submitContactHandler = (event) => {
     event.preventDefault();
 
-    if (formIsValid) {
+    if (validate()) {
       setIsLoading(true);
+      const { country, firstName, lastName, address, city } = values;
 
-      //  contact data
       const contactData = {
-        country: countryState.value,
-        firstName: firstNameState.value,
-        lastName: lastNameState.value,
-        address: addressState.value,
-        city: cityState.value,
+        country: country,
+        firstName: firstName,
+        lastName: lastName,
+        address: address,
+        city: city,
       };
 
       dispatch(contactActions.saveOrderData(contactData));
       setIsLoading(true);
       navigate("../payments", { replace: true });
-    } else if (!countryIsValid) {
-      countryInputRef.current.focus();
-    } else if (!firstNameIsValid) {
-      firstNameInputRef.current.focus();
-    } else if (!lastNameIsValid) {
-      lastNameInputRef.current.focus();
-    } else if (!addressIsValid) {
-      addressInputRef.current.focus();
-    } else {
-      cityInputRef.current.focus();
     }
   };
 
   return (
-    <form className={classes.orderform} onSubmit={submitContactHandler}>
-      <h3 className={classes.contact__title}>Contact Information</h3>
-      <Input
-        ref={countryInputRef}
-        id="country"
-        label="Country"
-        type="text"
-        isValid={countryIsValid}
-        value={countryState.value}
-        onChange={countryChangeHandler}
-        onBlur={validateCountryHandler}
-      />
+    <Fragment>
+      <div className={classes.form_div}>
+        <form
+          onSubmit={(event) => submitContactHandler(event)}
+          className={classes.form}
+        >
+          <div className={classes.brand}>
+            <h3 className={classes.app_heading}>Contact information</h3>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="country">Country</label>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="Country"
+              name="country"
+              onChange={handleChange}
+              label="country"
+            />
+          </div>
 
-      <Input
-        ref={firstNameInputRef}
-        id="firstname"
-        label="First name"
-        type="text"
-        isValid={firstNameIsValid}
-        value={firstNameState.value}
-        onChange={firstNameChangeHandler}
-        onBlur={validateFirstNameHandler}
-      />
+          <div className={classes.control}>
+            <label htmlFor="firstName">First name</label>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="First name"
+              name="firstName"
+              onChange={handleChange}
+            />
+          </div>
 
-      <Input
-        ref={lastNameInputRef}
-        id="lastname"
-        label="Last name"
-        type="text"
-        isValid={lastNameIsValid}
-        value={lastNameState.value}
-        onChange={lastNameChangeHandler}
-        onBlur={validateLastNameHandler}
-      />
+          <div className={classes.control}>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="Last name"
+              name="lastName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="address">Address</label>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="Address"
+              name="address"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="city">City</label>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="City"
+              name="city"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes.center__spin}>
+            {isLoading && <LoadingSpinner />}
+          </div>
 
-      <Input
-        ref={addressInputRef}
-        id="address"
-        label="Address"
-        type="text"
-        isValid={addressIsValid}
-        value={addressState.value}
-        onChange={addressChangeHandler}
-        onBlur={validateAddressHandler}
-      />
-
-      <Input
-        ref={cityInputRef}
-        id="city"
-        label="City"
-        type="text"
-        isValid={cityIsValid}
-        value={cityState.value}
-        onChange={cityChangeHandler}
-        onBlur={validateCityHandler}
-      />
-      {isLoading && <LoadingSpinner />}
-      <button>Submit</button>
-    </form>
+          <button type="submit" className={classes.button}>
+            Submit
+          </button>
+        </form>
+      </div>
+      <ToastContainer className={classes.toast} />
+    </Fragment>
   );
 };
 
