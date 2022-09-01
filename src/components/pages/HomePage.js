eebd6df2f8f15d1c78052de129a2cd560da44e34
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-01 16:26:26 +0300

    include new look and feel

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,17 @@
 import { Fragment, useState } from "react";
-// import { Link } from "react-router-dom";
 import classes from "./HomePage.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { useNavigate } from "react-router-dom";
+import logo from "./../../assets/paper.jpg";
 
 const HomePage = () => {
   const [showStoreElement, setShowStoreElement] = useState(true);
   const [showHelpElement, setShowHelpElement] = useState(true);
   const [showCompanyElement, setShowCompanyElement] = useState(true);
   const [showFindElement, setShowFindElements] = useState(true);
+
+  const navigate = useNavigate();
 
   const showStores = () => {
     setShowStoreElement((prevState) => !prevState);
@@ -53,13 +58,61 @@ const HomePage = () => {
     findIcon = "+";
   }
 
+  const productRoute =() => {
+    navigate("/products", true)
+  }
+
   return (
     <Fragment>
       <section className={classes.welcome_section}>
-        <p className={classes.mainheading}>
-          The store <span className={classes.word_heading}>for the</span>{" "}
-          perfect scarfs
-        </p>
+        <div className={classes.title_section}>
+          <img src={logo} className={classes.head_image} alt="start"/>
+          <p className={classes.mainheading}>
+            Bobo
+          </p>
+
+          <button onClick={productRoute} className={classes.shop_btn}>Shop Now</button>
+          
+        </div>
+      </section>
+
+      <div className={classes.title_carousel}>Featured products</div>
+
+      <section className={classes.middle_section}>
+        <div className={classes.carousel_wrapper}>
+          <Carousel
+            infiniteLoop
+            autoPlay
+            showArrows={true}
+            showThumbs={false}
+            showStatus={false}
+            centerSlidePercentage="80"
+            centerMode={false}
+            className={classes.card_carousel}
+          >
+            <div className={classes.image_container}>
+              <img
+                src="https://images.pexels.com/photos/7119090/pexels-photo-7119090.jpeg?auto=compress&cs=tinysrgb&w=600"
+                className={classes.carousel_image}
+                alt="shoes"
+              />
+            </div>
+            <div className={classes.image_container}>
+              <img
+                src="https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=600"
+                className={classes.carousel_image}
+                alt="bag"
+              />
+            </div>
+            <div className={classes.image_container}>
+              <img
+                src="https://images.pexels.com/photos/945698/pexels-photo-945698.jpeg?auto=compress&cs=tinysrgb&w=600"
+                className={classes.carousel_image}
+                alt="hankie"
+              />
+            </div>
+          </Carousel>
+        </div>
       </section>
 
       <footer className={classes.footer}>
