b9205f1fdfab98a35ff382349747dbc8a446cb43
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-03 02:04:56 +0300

    add list orders

--- src/components/pages/RealOrdersPage.js
+++ src/components/pages/RealOrdersPage.js
@@ -1,9 +1,22 @@
-import { useSelector } from "react-redux";
-import RealOrders from "../Orders/RealOrders";
-import classes from './RealOrdersPage.module.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchOrderData } from "../../store/order-actions";
+import { dateformatter } from "../../utils/timeConverter";
+import ListOrders from "../Orders/ListOrders";
+import classes from "./RealOrdersPage.module.css";
 
 const RealOrdersPage = () => {
   const orderItems = useSelector((state) => state.order.orders);
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+  const token = useSelector((state) => state.login.token);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+        dispatch(fetchOrderData(token));
+    }
+  }, [dispatch, isLoggedIn, token]);
 
   return (
     <div className={classes.real__orders}>
@@ -12,22 +25,26 @@ const RealOrdersPage = () => {
       {orderItems.length === 0 && (
         <p className={classes.empty}>No orders have been made as yet</p>
       )}
+
+      <div className={classes.table_titles}>
+        <span>OrderID</span>
+
+        <span className={classes.table_title_details}>
+          <p className={classes.created}>createdAt</p>
+          <p className={classes.status}>status</p>
+        </span>
+      </div>
       {orderItems.length > 0 &&
-        orderItems.map((item) =>
-          item.order.map((piece) => (
-            <RealOrders
-              key={piece.id}
-              id={piece.id}
-              image={piece.image}
-              description={piece.description}
-              title={piece.name}
-              price={piece.price}
-              quantity={piece.quantity}
-              totalPrice={piece.totalPrice}
-              status={item.status}
-            />
-          ))
-        )}
+        orderItems.map((item) => (
+          <ListOrders
+            key={item.id}
+            id={item.id}
+            totalPrice={item.totalPrice}
+            createdAt={dateformatter(item.createdAt)}
+            status={item.status}
+            payment_status={item.payment_status}
+          />
+        ))}
     </div>
   );
 };
