52f757dce9b06d933d2406bf5fbd8484cd10d5d8
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-03 02:00:15 +0300

    edit page

--- src/components/Orders/OrderDetail.js
+++ src/components/Orders/OrderDetail.js
@@ -2,16 +2,19 @@ import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import classes from "./OrderDetail.module.css";
 import { MdKeyboardArrowLeft } from "react-icons/md";
+import OrderListDetail from "./OrderListDetail";
 
 const timeConverter = (time) => {
   // converting firestore timestamp to normal date
   const date = new Date(time._seconds * 1000 + time._nanoseconds / 1000000);
 
-  //   const [month, day, year] = [
-  //     date.getMonth(),
-  //     date.getDate(),
-  //     date.getFullYear(),
-  //   ];
+  // const [month, day, year] = [
+  //   date.getMonth()+1,
+  //   date.getDate(),
+  //   date.getFullYear(),
+  // ];
+
+  // console.log(month, day, year)
   let [hour, minutes] = [date.getHours(), date.getMinutes()];
   if (hour === 0) {
     hour = hour + "0";
@@ -32,14 +35,17 @@ const OrderDetail = () => {
   const { orderId } = orderParams;
 
   // LOOPing through entire group collecting many orders to which order belongs
-  const group = orderItems.find((order) =>
-    order.order.some((item) => item.id === orderId)
-  );
+  // const group = orderItems.find((order) =>
+  //   order.order.some((item) => item.id === orderId)
+  // );
+
+  const group = orderItems.find((item) => item.id === orderId);
+  console.log(group);
 
   //   console.log(group.order);
 
   // getting specific order item
-  const order = group.order.find((item) => item.id === orderId);
+  // const order = group.order.find((item) => item.id === orderId);
   //   console.log(order);
 
   const createdAt = group.createdAt;
@@ -50,13 +56,15 @@ const OrderDetail = () => {
   // navigating backwards
   const previousPageHandler = () => {
     navigate(-1);
-  }
+  };
 
   return (
     <div className={classes.order__detail}>
       <div className={classes.header}>
-        <MdKeyboardArrowLeft onClick={previousPageHandler} className={classes.back_button}/>
-        <h3 className={classes.order__name}>{order.name}</h3>
+        <MdKeyboardArrowLeft
+          onClick={previousPageHandler}
+          className={classes.back_button}
+        />
       </div>
 
       <div className={classes.detail__page}>
@@ -65,14 +73,6 @@ const OrderDetail = () => {
           <p>Ordered: {finalDate}</p>
           <p>BOBO ID: {group.id}</p>
         </div>
-        <div className={classes.middle_div}>
-          <p>Your Bobo</p>
-          <div className={classes.pricing}>
-            <p>
-              {order.quantity}x {order.name} ${order.totalPrice}
-            </p>
-          </div>
-        </div>
         <div className={classes.delivery__details}>
           <h4>Delivery details</h4>
           <p>{group.address}</p>
@@ -80,6 +80,25 @@ const OrderDetail = () => {
           <p>{group.country}</p>
         </div>
       </div>
+      <div className={classes.order_titles}>
+        <p>Product</p>
+        <p>Name</p>
+        <p>Quantity</p>
+        <p>Price</p>
+      </div>
+
+      {group.order.map((piece) => (
+        <OrderListDetail
+          key={piece.id}
+          id={piece.id}
+          image={piece.image}
+          description={piece.description}
+          title={piece.name}
+          price={piece.price}
+          quantity={piece.quantity}
+          totalPrice={piece.totalPrice}
+        />
+      ))}
     </div>
   );
 };
